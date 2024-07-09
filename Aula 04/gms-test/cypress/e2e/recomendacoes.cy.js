864d38bd079aaa72fe746973f41c9356e9a09721
Amanda da Croce <dacroce.a07@gmail.com>
2024-07-08 22:53:51 -0300

    Refatoração e teste de busca

---
+++ Aula 04/gms-test/cypress/e2e/recomendacoes.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress"/>
+
+describe('US-015-Recomendações Diárias de Filmes ', () => {
+
+    beforeEach(() => {
+      cy.visit('/')
+    });
+
+    it('Deve fazer a exibição de recomendações diárias de filmes em alta', () => {
+      cy.get('#recommendations')
+        .find('img')  // Seleciona todas as imagens dentro da div
+        .should(($imgs) => {
+          // Verifica se o número de imagens está entre 4 e 5
+          expect($imgs).to.have.length.within(4, 5)
+        });
+    });
+})
